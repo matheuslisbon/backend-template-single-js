49dfa2b05912a08bc6b1fa922095ae138ee63382
Matheus <mastheus@bemaxgroup.com.br>
2025-03-23 01:13:19 -0300

    [feat] add readme and dev config

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express'
 const app = express()
-
+import cors from 'cors'
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors('*'))
 const port = 3000
 
 app.get('/', (req, res) => {
-  console.log(req)
-
-  return res.status(200).send('hey')
+  return res.status(200).send('hello world')
 })
 
 app.listen(port, () => {
